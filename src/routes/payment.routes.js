65637cddcd07bf14ed1da770144a0e7c3cff273a
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-12 23:05:08 +0100

    schema for payment implemented

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { initializePayment, verifyPayment } from '../controllers/payment.controller.js';
 import handleAsyncErr from '../utils/catchAsync.js'
+import isLoggedin from '../utils/isLoggedin.js';
 
 const router = express.Router();
 
 // Route to initialize payment
-router.post('/initialize', handleAsyncErr(initializePayment));
+router.post('/initialize',  handleAsyncErr(initializePayment));
 
 // Route to verify payment
-router.post('/verify', handleAsyncErr(verifyPayment));
+router.post('/verify',  handleAsyncErr(verifyPayment));
 
 export default router;
