ac674ebdf1c27d90b559d98b01f3bb881896aa58
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-12 18:52:51 +0100

    order schema and paystack payment controller implemented

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+// Initialize payment with Paystack
+export const initializePayment = async (req, res) => {
+    const { email, amount } = req.body; // Get email and amount from the request body
+    const paystackSecretKey = process.env.PAYSTACK_SECRET_KEY;
+
+    // Make request to Paystack to initialize payment
+    const response = await axios.post(
+        'https://api.paystack.co/transaction/initialize',
+        {
+            email,
+            amount: amount * 100,
+            callback_url: 'http://localhost:3000/verify'
+        }, // Paystack expects amount in kobo
+        {
+            headers: {
+                Authorization: `Bearer ${paystackSecretKey}`,
+                'Content-Type': 'application/json',
+            },
+        }
+    );
+    res.status(200).json(response.data);
+};
+
+
+
+// Verify payment with Paystack
+export const verifyPayment = async (req, res) => {
+    const { reference } = req.body; // Retrieve reference from cookie
+    if (!reference) return res.status(400).json({ error: 'No reference found' });
+
+    const paystackSecretKey = process.env.PAYSTACK_SECRET_KEY;
+
+    // Verify the transaction using the reference from the cookie
+    const response = await axios.get(
+        `https://api.paystack.co/transaction/verify/${reference}`,
+        {
+            headers: { Authorization: `Bearer ${paystackSecretKey}` },
+        }
+    );
+
+    const { data } = response.data;
+    if (data.status === 'success') {
+        // Handle successful payment (e.g., save order to database)
+        return res.status(200).json({ success: true, data });
+    } else {
+        return res.status(400).json({ success: false, message: 'Payment not successful' });
+    }
+}
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -3,48 +3,48 @@ const { Schema } = mongoose;
 
 // Product Schema
 const productSchema = new Schema({
-     name: {
-            type: String,
-            required: true,
-            trim: true, // Trim whitespace from the name
-        },
-        images: {
-            type: [String], // Array of image URLs
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-            min: [0, 'Price cannot be negative'], // Ensure price is non-negative
-        },
-        sizes: {
-            type: [String], // Array of available sizes (e.g., ["S", "M", "L", "XL"])
-            required: true,
-        },
-        colors: {
-            type: [String], // Flexible array of colors (e.g., ["red", "green", "blue"])
-            required: true,
-        },
-        quantity: {
-            type: Number, // Number of items available
-            required: true,
-            min: [0, 'Quantity cannot be negative'],
-        },
-        categoryId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Category', // Reference to the Categories collection
-            required: true,
-        },
-        gender: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Gender', // Reference to the Gender schema
-            required: true,
-        },
+    name: {
+        type: String,
+        required: true,
+        trim: true, // Trim whitespace from the name
     },
+    images: {
+        type: [String], // Array of image URLs
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price cannot be negative'], // Ensure price is non-negative
+    },
+    sizes: {
+        type: [String], // Array of available sizes (e.g., ["S", "M", "L", "XL"])
+        required: true,
+    },
+    colors: {
+        type: [String], // Flexible array of colors (e.g., ["red", "green", "blue"])
+        required: true,
+    },
+    quantity: {
+        type: Number, // Number of items available
+        required: true,
+        min: [0, 'Quantity cannot be negative'],
+    },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category', // Reference to the Categories collection
+        required: true,
+    },
+    gender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Gender', // Reference to the Gender schema
+        required: true,
+    },
+},
     {
         timestamps: { createdAt: 'createdAt' }, // Automatically adds createdAt timestamp
     }
