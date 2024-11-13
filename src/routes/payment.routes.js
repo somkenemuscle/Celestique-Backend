812bae1be917462cd6c80c63dd90571d9995c6b1
Somkene Ojukwu <somkeneoj@gmail.com>
2024-11-13 22:25:54 +0100

    cart route and cart model implemented

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -6,9 +6,9 @@ import isLoggedin from '../utils/isLoggedin.js';
 const router = express.Router();
 
 // Route to initialize payment
-router.post('/initialize',  handleAsyncErr(initializePayment));
+router.post('/initialize', isLoggedin, handleAsyncErr(initializePayment));
 
 // Route to verify payment
-router.post('/verify',  handleAsyncErr(verifyPayment));
+router.post('/verify', isLoggedin, handleAsyncErr(verifyPayment));
 
 export default router;
