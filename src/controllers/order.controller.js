c1cc997c2a457cc4def7d4d940e4b56b2772a7b6
zainab-kassim <kassimzainab11@gmail.com>
2024-12-06 13:54:46 +0100

    search route implimented

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -17,3 +17,20 @@ export const getUserOrders = async (req, res) => {
     res.status(200).json({ success: true, orders });
 
 };
+
+// Fetch orders for the authenticated user
+export const getUserOrderDetails = async (req, res) => {
+    const OrderId=req.params.orderId
+
+    // Fetch orders belonging to the user and sort by creation date (latest first)
+    const orders = await Order.findById({ _id: OrderId })
+        .sort({ createdAt: -1 })
+        .populate('items.product', 'name price images'); // Populate product details if needed
+    
+    if (orders.length === 0) {
+        return res.status(404).json({ message: 'No orders found' });
+    }
+
+    res.status(200).json({ success: true, orders });
+
+};
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserOrders } from '../controllers/order.controller.js';
+import { getUserOrderDetails, getUserOrders } from '../controllers/order.controller.js';
 import handleAsyncErr from '../utils/catchAsync.js'
 import isLoggedin from '../utils/isLoggedin.js';
 
@@ -7,5 +7,8 @@ const router = express.Router();
 
 // Get the user's Orders
 router.get('/', isLoggedin, handleAsyncErr(getUserOrders));
+
+// Get the user's Orders
+router.get('/:orderId',isLoggedin, handleAsyncErr(getUserOrderDetails))
 
 export default router;
