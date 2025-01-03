35015264799cd16bd991b5c2bcf5d55948785872
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-03 15:18:53 +0100

    Working on deployment

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -6,8 +6,9 @@ dotenv.config();
 
 // CORS Middleware Configuration
 const corsMiddleware = cors({
-  origin: `${process.env.VERCEL_FRONTEND_URL}`, // Replace with your frontend URL
+  // origin: `${process.env.VERCEL_FRONTEND_URL}`, // Replace with your frontend URL
   // origin: process.env.VERCEL_FRONTEND_URL || 'http://localhost:3000', // Use environment variable or localhost
+   origin: 'https://celestique.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
