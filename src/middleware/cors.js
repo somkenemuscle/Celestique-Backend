4796b77cfb06b60e22ed54d561c081395036eda8
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-03 15:02:22 +0100

    Working on deployment

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -6,7 +6,8 @@ dotenv.config();
 
 // CORS Middleware Configuration
 const corsMiddleware = cors({
-  origin: process.env.VERCEL_FRONTEND_URL || 'http://localhost:3000', // Use environment variable or localhost
+  origin: `${process.env.VERCEL_FRONTEND_URL}`, // Replace with your frontend URL
+  // origin: process.env.VERCEL_FRONTEND_URL || 'http://localhost:3000', // Use environment variable or localhost
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
