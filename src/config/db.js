d108f3f1520c3387145e4633c9dfa78ec86e1f73
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-14 21:28:49 +0100

    Envs Updated

--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,15 @@ import mongoose from 'mongoose';
 export default function connectDb() {
 
     // Get the MongoDB connection URL from environment variables
+    const dbUrl = 'mongodb+srv://somkene:muscle$$090@celestiquecluster.uwhws.mongodb.net/?retryWrites=true&w=majority&appName=CelestiqueCluster';
 
-    if (!process.env.DB_URL) {
+    if (!dbUrl) {
         console.error('Database URL is not defined in the environment variables');
         process.exit(1); // Exit the process with an error code
     }
 
     // Connect to MongoDB without deprecated options
-    mongoose.connect(process.env.DB_URL)
+    mongoose.connect(dbUrl)
         .then(() => console.log('Celestique app has connected to the database'))
         .catch(err => {
             console.error('Error connecting to database:', err);
