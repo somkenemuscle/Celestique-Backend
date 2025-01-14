528003de4c4cf54feea4764747a3b070f42f4803
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-14 20:27:15 +0100

    Envs Updated

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 export default function connectDb() {
     // Get the MongoDB connection URL from environment variables
-    const dbUrl = 'mongodb+srv://somkene:muscle$$090@celestiquecluster.uwhws.mongodb.net/?retryWrites=true&w=majority&appName=CelestiqueCluster';
+    const dbUrl = process.env.DB_URL;
 
     if (!dbUrl) {
         console.error('Database URL is not defined in the environment variables');
@@ -17,9 +17,9 @@ export default function connectDb() {
 
     // Connect to MongoDB without deprecated options
     mongoose.connect(dbUrl)
-    .then(() => console.log('Celestique app has connected to the database'))
-    .catch(err => {
-        console.error('Error connecting to database:', err);
-        process.exit(1); // Exit the process with an error code on failure
-    });
+        .then(() => console.log('Celestique app has connected to the database'))
+        .catch(err => {
+            console.error('Error connecting to database:', err);
+            process.exit(1); // Exit the process with an error code on failure
+        });
 }
--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,14 +1,8 @@
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 // CORS Middleware Configuration
 const corsMiddleware = cors({
-  // origin: `${process.env.VERCEL_FRONTEND_URL}`, // Replace with your frontend URL
-  // origin: process.env.VERCEL_FRONTEND_URL || 'http://localhost:3000', // Use environment variable or localhost
-   origin: 'https://celestique.vercel.app',
+  origin: process.env.VERCEL_FRONTEND_URL, // Use environment variable or localhost
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
